70a3c39a6e790847a586e8a4f5730d219db5698b
Trung Pham <pbtrung@outlook.com>
2026-02-22 11:25:54 +0700

    Remove legacy web assets and keep repository Rust-only

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import './index.css';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-createRoot(document.getElementById('root')).render(<App />);
